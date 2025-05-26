74cd64de29c84cf35ad5de37cc4989b4c8c94410
RadhaBadgujar <radhabadgujar27@gmail.com>
2025-05-26 19:44:57 +0530

    Added SpringBootJobApp1 project

---
+++ SpringBootJobApp1/src/main/java/com/example/SpringBootJobApp1/SpringBootJobApp1Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootJobApp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJobApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJobApp1Application.class, args);
+	}
+
+}
---
+++ SpringBootJobApp1/src/test/java/com/example/SpringBootJobApp1/SpringBootJobApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootJobApp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJobApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
